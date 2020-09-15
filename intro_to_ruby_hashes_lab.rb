3140a03edee22087dad9617caa90ec667a79b5c8
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-15 19:32:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,8 @@ monopoly = {:railroads =>
               {
                 :reading_railroad => {},
                 :pennsylvania_railroad  => {},
-                :b_and_o_railroad => {}
+                :b_and_o_railroad => {},
+                :shortline_railroad => {}
               }
             }
           }
