6449db72c6ccc509597c42e2b988488cc7d31148
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-15 19:29:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,8 @@ monopoly = {:railroads =>
               {
                 :one_piece_owned => 25,
                 :two_pieces_owned => 50,
-                :three_pieces_owned => 100
+                :three_pieces_owned => 100,
+                :four_pieces_owned => 200
               },
               :names => {}
             }
