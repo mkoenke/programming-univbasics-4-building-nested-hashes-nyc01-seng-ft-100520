4784ea262f97d31850a5f80cfff0aea50bf7c4c6
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-15 19:23:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,8 +26,7 @@ def monopoly_with_third_tier
 monopoly = {:railroads => 
         {:pieces => 4, 
         :rent_in_dollars  => {
-          :one_piece_owned => 25
-          }
+          :one_piece_owned => 25}
         },
         :names => {}
         }
